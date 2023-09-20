4213f933f39133599cebf1fe99166dc2962d565f
zszdmzmm <1075534380@qq.com>
2023-09-20 18:39:07 +0800

    @BeforeAll @AfterAll

--- day0915-spring-quickstart/src/test/java/com/aue/SpringStartTest.java
+++ day0915-spring-quickstart/src/test/java/com/aue/SpringStartTest.java
@@ -1,19 +1,23 @@
 package com.aue;
 
 import com.aue.dao.impl.SimpleUserDao;
-import com.aue.service.impl.InitAndDestroyXMLUserService;
 import com.aue.service.impl.InitAndDestroyUserService;
+import com.aue.service.impl.InitAndDestroyXMLUserService;
 import com.aue.service.impl.SimpleUserService;
-import org.junit.jupiter.api.Assertions;
-import org.junit.jupiter.api.DisplayName;
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.*;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 public class SpringStartTest {
+    static ClassPathXmlApplicationContext ac;
+
+    @BeforeAll
+    public static void beforeTest() {
+        ac = new ClassPathXmlApplicationContext("bean.xml");
+    }
+
     @Test
     @DisplayName("spring容器里存在，不存在哪些bean")
     public void IoCTest() {
-        ClassPathXmlApplicationContext ac = new ClassPathXmlApplicationContext("bean.xml");
         Assertions.assertTrue(ac.containsBean("userService"));
         Assertions.assertTrue(ac.containsBean("userDao"));
         Assertions.assertFalse(ac.containsBean("itDoseNotExist"));
@@ -24,7 +28,6 @@ public class SpringStartTest {
     @Test
     @DisplayName("spring初始化了哪些类型")
     public void IoCTest1() {
-        ClassPathXmlApplicationContext ac = new ClassPathXmlApplicationContext("bean.xml");
         Assertions.assertInstanceOf(SimpleUserService.class, ac.getBean("userService"));
         Assertions.assertInstanceOf(SimpleUserDao.class, ac.getBean("userDao"));
     }
@@ -32,7 +35,6 @@ public class SpringStartTest {
     @Test
     @DisplayName("通过配置文件与setter方法向容器注入依赖")
     public void DITest() {
-        ClassPathXmlApplicationContext ac = new ClassPathXmlApplicationContext("bean.xml");
         SimpleUserService simpleUserService = (SimpleUserService) ac.getBean("userService");
         SimpleUserService simpleUserService1 = (SimpleUserService) ac.getBean("userService1");
         Assertions.assertNull(simpleUserService.getUserDao());
@@ -42,7 +44,6 @@ public class SpringStartTest {
     @Test
     @DisplayName("通过配置文件与构造方法向容器注入依赖")
     public void DITest1() {
-        ClassPathXmlApplicationContext ac = new ClassPathXmlApplicationContext("bean.xml");
         SimpleUserService simpleUserService = (SimpleUserService) ac.getBean("userService2");
         Assertions.assertInstanceOf(SimpleUserDao.class, simpleUserService.getUserDao());
     }
@@ -50,9 +51,7 @@ public class SpringStartTest {
     @Test
     @DisplayName("只写了init和destroy方法，不做处理，无法被调用")
     public void InitAndDestroyTest1() {
-        ClassPathXmlApplicationContext ac = new ClassPathXmlApplicationContext("bean.xml");
-        InitAndDestroyUserService userService3 =(InitAndDestroyUserService) ac.getBean("userService3");
-        ac.close();
+        InitAndDestroyUserService userService3 = (InitAndDestroyUserService) ac.getBean("userService3");
         //控制台未打出InitAndDestroyUserService.init, InitAndDestroyUserService.destroy
         Assertions.assertFalse(false);
     }
@@ -60,10 +59,13 @@ public class SpringStartTest {
     @Test
     @DisplayName("只写了init和destroy方法，配置中加入指定方法, 控制台打出InitAndDestroyUserService.init, InitAndDestroyUserService.destroy")
     public void InitAndDestroyTest2() {
-        ClassPathXmlApplicationContext ac = new ClassPathXmlApplicationContext("bean.xml");
-        InitAndDestroyXMLUserService userService4 =(InitAndDestroyXMLUserService) ac.getBean("userService4");
-        ac.close();
+        InitAndDestroyXMLUserService userService4 = (InitAndDestroyXMLUserService) ac.getBean("userService4");
         //控制台打出InitAndDestroyUserService.init, InitAndDestroyUserService.destroy
         Assertions.assertTrue(true);
+    }
+
+    @AfterAll
+    public static void afterTest() {
+        ac.close();
     }
 }
