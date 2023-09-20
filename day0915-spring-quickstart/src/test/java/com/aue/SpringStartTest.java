40da5146e44972b45d8c7458a598bb9623ade9ad
zszdmzmm <1075534380@qq.com>
2023-09-20 16:59:00 +0800

    DI Setter-based dependency injection

--- day0915-spring-quickstart/src/test/java/com/aue/SpringStartTest.java
+++ day0915-spring-quickstart/src/test/java/com/aue/SpringStartTest.java
@@ -1,9 +1,9 @@
 package com.aue;
 
+import com.aue.dao.UserDao;
 import com.aue.dao.impl.SimpleUserDao;
 import com.aue.service.impl.SimpleUserService;
 import org.junit.jupiter.api.Assertions;
-
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
@@ -26,5 +26,15 @@ public class SpringStartTest {
         ClassPathXmlApplicationContext ac = new ClassPathXmlApplicationContext("bean.xml");
         Assertions.assertInstanceOf(SimpleUserService.class, ac.getBean("userService"));
         Assertions.assertInstanceOf(SimpleUserDao.class, ac.getBean("userDao"));
+    }
+
+    @Test
+    @DisplayName("通过配置文件与setter方法向容器注入依赖")
+    public void DITest() {
+        ClassPathXmlApplicationContext ac = new ClassPathXmlApplicationContext("bean.xml");
+        SimpleUserService simpleUserService = (SimpleUserService) ac.getBean("userService");
+        SimpleUserService simpleUserService1 = (SimpleUserService) ac.getBean("userService1");
+        Assertions.assertNull(simpleUserService.getUserDao());
+        Assertions.assertInstanceOf(SimpleUserDao.class, simpleUserService1.getUserDao());
     }
 }
