b77db7a0f08d294a9ab84687d4b545b5b5a8f8ca
zszdmzmm <1075534380@qq.com>
2023-09-28 11:22:19 +0800

    helloSpringWebMvc webConfig

---
+++ day0928-spring-mvc/src/main/java/com/aue/MyWebApplicationInitializer.java
@@ -0,0 +1,22 @@
+package com.aue;
+
+import com.aue.config.MyWebConfig;
+import org.springframework.web.servlet.support.AbstractAnnotationConfigDispatcherServletInitializer;
+
+public class MyWebApplicationInitializer extends AbstractAnnotationConfigDispatcherServletInitializer {
+
+    @Override
+    protected Class<?>[] getRootConfigClasses() {
+        return new Class[0];
+    }
+
+    @Override
+    protected Class<?>[] getServletConfigClasses() {
+        return new Class[]{MyWebConfig.class};
+    }
+
+    @Override
+    protected String[] getServletMappings() {
+        return new String[] {"/"};
+    }
+}
