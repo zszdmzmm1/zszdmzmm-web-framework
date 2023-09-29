592b92618f1cb9eed260430e2b959266bdfe0206
zszdmzmm <1075534380@qq.com>
2023-09-29 14:48:13 +0800

    request/mapping-method-get

--- day0928-spring-mvc/src/main/java/com/aue/controller/HelloController.java
+++ day0928-spring-mvc/src/main/java/com/aue/controller/HelloController.java
@@ -5,10 +5,7 @@ import com.aue.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.format.annotation.DateTimeFormat;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.ArrayList;
 import java.util.Date;
@@ -87,5 +84,11 @@ public class HelloController {
         userList.add(user);
         userList.add(user1);
         return userList;
+    }
+
+    @RequestMapping(path = "request/mapping-method-get", method = RequestMethod.GET)
+    @ResponseBody
+    public String mappingMethodGet(String method) {
+        return "Method:" + method;
     }
 }
