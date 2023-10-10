24640f0e71587b0c82d53687a9413b299bdaa0c1
zszdmzmm <1075534380@qq.com>
2023-10-10 17:06:32 +0800

    @JsonProperty(access = JsonProperty.Access.WRITE_ONLY)

--- day1010-common-response/src/main/java/com/aue/pojo/User.java
+++ day1010-common-response/src/main/java/com/aue/pojo/User.java
@@ -1,6 +1,7 @@
 package com.aue.pojo;
 
 import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.fasterxml.jackson.annotation.JsonProperty;
 import jakarta.validation.constraints.Email;
 import lombok.Data;
 import org.hibernate.validator.constraints.Length;
@@ -14,7 +15,7 @@ public class User {
     private String email;
 
     @Length(min = 4, max = 16)
-    //@JsonIgnore
+    @JsonProperty(access = JsonProperty.Access.WRITE_ONLY)
     private String password;
     private String role;
 
