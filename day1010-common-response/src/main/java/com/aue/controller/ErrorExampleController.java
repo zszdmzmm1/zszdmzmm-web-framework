f9f47a58388c7d4fdc30fa2cc4b28f66a9874c06
zszdmzmm <1075534380@qq.com>
2023-10-10 11:47:17 +0800

    Exception 未处理

---
+++ day1010-common-response/src/main/java/com/aue/controller/ErrorExampleController.java
@@ -0,0 +1,16 @@
+package com.aue.controller;
+
+import com.aue.pojo.R;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+public class ErrorExampleController {
+    @GetMapping("error")
+    @ResponseBody
+    public R errorExample() {
+        int i = 1 / 0;
+        return R.ok("1/0");
+    }
+}
