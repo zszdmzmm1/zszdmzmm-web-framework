c97647e5d79a81903e48a3c65288b1b9795b80ae
zszdmzmm <1075534380@qq.com>
2023-10-10 16:42:30 +0800

    @Email with custom message

--- day1010-common-response/src/main/java/com/aue/pojo/User.java
+++ day1010-common-response/src/main/java/com/aue/pojo/User.java
@@ -10,7 +10,7 @@ import java.util.List;
 @Data
 public class User {
     private int id;
-    @Email
+    @Email(message = "自定义消息：错误的邮箱格式")
     private String email;
 
     @Length(min = 4, max = 16)
